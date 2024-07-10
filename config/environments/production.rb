c5ef0b425fbceecdc5890bf84ddf1ca8e1e4a7ac
maxim.joseau <maxim.joseau@ouidou.fr>
2024-07-10 13:51:39 +0200

    fix : Enable harmony mode in Uglifier and add Node.js buildpack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,7 @@ Rails.application.configure do
   config.public_file_server.headers = {
   'Cache-Control' => 'public, max-age=31536000'
   }
-  config.assets.js_compressor = :uglifier
-
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
